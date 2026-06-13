51a69efa7cdc96b116d9d897f7f68a674805d522
Zaahid-7 <zahid280504@gmail.com>
2026-06-13 12:21:06 +0600

    Fork monitor with PID information

--- fork_monitor.py
+++ fork_monitor.py
@@ -1,15 +1,17 @@
 from bcc import BPF
 
 program = """
+#include <uapi/linux/ptrace.h>
+
 TRACEPOINT_PROBE(sched, sched_process_fork)
 {
-    bpf_trace_printk("Fork detected\\n");
+    bpf_trace_printk("Fork: parent=%d child=%d\\n", args->parent_pid, args->child_pid);
     return 0;
 }
 """
 
 b = BPF(text=program)
 
-print("Listening for fork events...")
+print("Listening for fork events with PID info...")
 
 b.trace_print()
